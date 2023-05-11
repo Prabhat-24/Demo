bb7fcca1b093de5fb50027982a0700488d96416a
Prabhat-24 <130762956+Prabhat-24@users.noreply.github.com>
2023-05-11 10:01:35 +0530

    Update Erc1155Storage.sol

--- Erc1155MarketPlace/Erc1155Storage.sol
+++ Erc1155MarketPlace/Erc1155Storage.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "NFT/OLD/String.sol";
 
-interface IERC1155Receiver {
-    function onERC1155Received(
-        address operator,
-        address from,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) external pure returns (bytes4);
+contract Erc1155Storage {
+    string internal baseURI;
+    mapping(uint256 => string) private _tokenUri;
+
+    function setBaseURI(string memory _baseURI) internal {
+        baseURI = _baseURI;
+    }
+
+    function setTokenURI(uint256 _numberOfId) internal {
+        _tokenUri[_numberOfId] = string.concat(
+            baseURI,
+            Strings.toString(_numberOfId),
+            ".json"
+        );
+    }
+
+    function tokenURI(uint256 _id) internal view returns (string memory) {
+        return _tokenUri[_id];
+    }
 }
